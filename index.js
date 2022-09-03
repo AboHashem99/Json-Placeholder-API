371b086c2fbf71d3bfd1a36583b8c86821d0398d
AboHashem99 <a.abohashem22@gmail.com>
2022-09-04 00:34:59 +0300

    Functionally Efficient

--- index.js
+++ index.js
@@ -1,204 +1,9 @@
+// from 2-6 to connect to API
 let request = new XMLHttpRequest();
 request.open("GET", "https://jsonplaceholder.typicode.com/users");
 request.responseType="json";
 request.send();
-request.onload = function(){
-    let request2 = new XMLHttpRequest();
-    request2.open("GET", "https://jsonplaceholder.typicode.com/posts");
-    request2.responseType="json";
-    request2.send();
-    request2.onload = function(){
-        let posts = request2.response;
-        var array = []; 
-        for (post of posts) {
-            array.push([post.id, post.userId, post.title, post.body]);
-        }
-        let list = document.getElementById("container2");
-        for (let index = 0; index < array.length; index++) {
-            if (array[index][1]== "1") {
-                
-                let item = document.createElement("div");
-                let title = document.createElement("h4");
-                let body = document.createElement("p");
-                item.className = "list-posts";
-                title.innerHTML= array[index][2] + "<hr>";
-                body.innerHTML = array[index][3];
-                item.append(title);
-                item.append(body);
-                list.append(item);
-            }
-        }
-        document.getElementById("item1").addEventListener("click", function(){
-            list.innerHTML="";
-            for (let index = 0; index < array.length; index++) {
-                if (array[index][1]== "1") {
-                    
-                    let item = document.createElement("div");
-                    let title = document.createElement("h4");
-                    let body = document.createElement("p");
-                    item.className = "list-posts";
-                    title.innerHTML= array[index][2] + "<hr>";
-                    body.innerHTML = array[index][3];
-                    item.append(title);
-                    item.append(body);
-                    list.append(item);
-                }
-            }
-        })
-        document.getElementById("item2").addEventListener("click", function(){
-            list.innerHTML="";
-            for (let index = 0; index < array.length; index++) {
-                if (array[index][1]== "2") {
-                    
-                    let item = document.createElement("div");
-                    let title = document.createElement("h4");
-                    let body = document.createElement("p");
-                    item.className = "list-posts";
-                    title.innerHTML= array[index][2] + "<hr>";
-                    body.innerHTML = array[index][3];
-                    item.append(title);
-                    item.append(body);
-                    list.append(item);
-                }
-            }
-        })
-        document.getElementById("item3").addEventListener("click", function(){
-            list.innerHTML="";
-            for (let index = 0; index < array.length; index++) {
-                if (array[index][1]== "3") {
-                    
-                    let item = document.createElement("div");
-                    let title = document.createElement("h4");
-                    let body = document.createElement("p");
-                    item.className = "list-posts";
-                    title.innerHTML= array[index][2] + "<hr>";
-                    body.innerHTML = array[index][3];
-                    item.append(title);
-                    item.append(body);
-                    list.append(item);
-                }
-            }
-        })
-        document.getElementById("item4").addEventListener("click", function(){
-            list.innerHTML="";
-            for (let index = 0; index < array.length; index++) {
-                if (array[index][1]== "4") {
-                    
-                    let item = document.createElement("div");
-                    let title = document.createElement("h4");
-                    let body = document.createElement("p");
-                    item.className = "list-posts";
-                    title.innerHTML= array[index][2] + "<hr>";
-                    body.innerHTML = array[index][3];
-                    item.append(title);
-                    item.append(body);
-                    list.append(item);
-                }
-            }
-        })
-        document.getElementById("item5").addEventListener("click", function(){
-            list.innerHTML="";
-            for (let index = 0; index < array.length; index++) {
-                if (array[index][1]== "5") {
-                    
-                    let item = document.createElement("div");
-                    let title = document.createElement("h4");
-                    let body = document.createElement("p");
-                    item.className = "list-posts";
-                    title.innerHTML= array[index][2] + "<hr>";
-                    body.innerHTML = array[index][3];
-                    item.append(title);
-                    item.append(body);
-                    list.append(item);
-                }
-            }
-        })
-        document.getElementById("item6").addEventListener("click", function(){
-            list.innerHTML="";
-            for (let index = 0; index < array.length; index++) {
-                if (array[index][1]== "6") {
-                    
-                    let item = document.createElement("div");
-                    let title = document.createElement("h4");
-                    let body = document.createElement("p");
-                    item.className = "list-posts";
-                    title.innerHTML= array[index][2] + "<hr>";
-                    body.innerHTML = array[index][3];
-                    item.append(title);
-                    item.append(body);
-                    list.append(item);
-                }
-            }
-        })
-        document.getElementById("item7").addEventListener("click", function(){
-            list.innerHTML="";
-            for (let index = 0; index < array.length; index++) {
-                if (array[index][1]== "7") {
-                    
-                    let item = document.createElement("div");
-                    let title = document.createElement("h4");
-                    let body = document.createElement("p");
-                    item.className = "list-posts";
-                    title.innerHTML= array[index][2] + "<hr>";
-                    body.innerHTML = array[index][3];
-                    item.append(title);
-                    item.append(body);
-                    list.append(item);
-                }
-            }
-        })
-        document.getElementById("item8").addEventListener("click", function(){
-            list.innerHTML="";
-            for (let index = 0; index < array.length; index++) {
-                if (array[index][1]== "8") {
-                    
-                    let item = document.createElement("div");
-                    let title = document.createElement("h4");
-                    let body = document.createElement("p");
-                    item.className = "list-posts";
-                    title.innerHTML= array[index][2] + "<hr>";
-                    body.innerHTML = array[index][3];
-                    item.append(title);
-                    item.append(body);
-                    list.append(item);
-                }
-            }
-        })
-        document.getElementById("item9").addEventListener("click", function(){
-            list.innerHTML="";
-            for (let index = 0; index < array.length; index++) {
-                if (array[index][1]== "9") {
-                    
-                    let item = document.createElement("div");
-                    let title = document.createElement("h4");
-                    let body = document.createElement("p");
-                    item.className = "list-posts";
-                    title.innerHTML= array[index][2] + "<hr>";
-                    body.innerHTML = array[index][3];
-                    item.append(title);
-                    item.append(body);
-                    list.append(item);
-                }
-            }
-        })
-        document.getElementById("item10").addEventListener("click", function(){
-            list.innerHTML="";
-            for (let index = 0; index < array.length; index++) {
-                if (array[index][1]== "10") {
-                    
-                    let item = document.createElement("div");
-                    let title = document.createElement("h4");
-                    let body = document.createElement("p");
-                    item.className = "list-posts";
-                    title.innerHTML= array[index][2] + "<hr>";
-                    body.innerHTML = array[index][3];
-                    item.append(title);
-                    item.append(body);
-                    list.append(item);
-                }
-            }
-        })
-    }   
+request.onload = function(){  // 6-22 add users to page
     let users = request.response;
     let userIds = [];
     for(user of users){
@@ -215,4 +20,52 @@ request.onload = function(){
         item.append(email);
         list.append(item);   
     }
+    let request2 = new XMLHttpRequest();  // 23-27 call posts
+    request2.open("GET", "https://jsonplaceholder.typicode.com/posts");
+    request2.responseType="json";
+    request2.send();
+    request2.onload = function(){
+        let posts = request2.response;
+        var array = [];   // 29-32 add posts to array  ( multidimensional )
+        for (post of posts) {
+            array.push([post.id, post.userId, post.title, post.body]);
+        }
+        let list = document.getElementById("container2");
+        for (let index = 0; index < array.length; index++) {   // 34-46 add posts of the first user to page
+            if (array[index][1]== "1") {
+                let item = document.createElement("div");
+                let title = document.createElement("h4");
+                let body = document.createElement("p");
+                item.className = "list-posts";
+                title.innerHTML= array[index][2] + "<hr>";
+                body.innerHTML = array[index][3];
+                item.append(title);
+                item.append(body);
+                list.append(item);
+            }
+        }
+        var hh = document.getElementsByClassName("list-users")
+        for (let i = 0; i < hh.length; i++) {
+            for (let j = 0; j < userIds.length; j++) {
+                if (hh[i].id=="item"+userIds[j]) {   // compare html element id to user id
+                    hh[i].addEventListener("click", function(){
+                        list.innerHTML="";   // remove the prev user's posts
+                        for (let index = 0; index < array.length; index++) {   // if html id == user id add posts
+                            if (array[index][1]== userIds[j]) {
+                                let item = document.createElement("div");
+                                let title = document.createElement("h4");
+                                let body = document.createElement("p");
+                                item.className = "list-posts";
+                                title.innerHTML= array[index][2] + "<hr>";
+                                body.innerHTML = array[index][3];
+                                item.append(title);
+                                item.append(body);
+                                list.append(item);
+                            }
+                        }
+                    })
+                }   
+            }   
+        }
+    }    
 }
